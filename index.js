0bbfee49d84d7db78db9628ebfc8768d23f81e19
Marc DeMory <marcdemory95@gmail.com>
2017-10-03 16:52:13 -0500

    trying to include dependencies

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
+let OccasionClient = new Occasion.Client('463436b5ebf74b42873aaa544801f91a');
+
 var app = angular.module('StickyBooking', []);
 
 app.controller('BookingController', function($scope, $http) {
 
     //OnInit
     this.$onInit = function(){
-        alert("Hey");
+        
     }
 
     //Function to load field data from database
     $scope.testFunction = function(){
-        alert("there");
+    
     }
 
 });
