1f493fcec81a1714db26b50edeca662a59384126
Nick Landgrebe <nick@landgre.be>
2017-11-07 16:13:34 -0600

    Use specific commit of occasion-sdk to fix broken price calculation

--- require/require-config.js
+++ require/require-config.js
@@ -19,7 +19,7 @@ require.config({
         "underscore.string": '/node_modules/underscore.string/dist/underscore.string',
         "underscore.inflection": '/node_modules/underscore.inflection/lib/underscore.inflection',
         "active-resource": '/node_modules/active-resource/build/active-resource.min',
-        "occasion-sdk": '/node_modules/occasion-sdk/dist/occasion-sdk.min',
+        "occasion-sdk": '/node_modules/occasion-sdk/build/occasion-sdk.min',
         "angular": '/node_modules/angular/angular.min'
     }
 });
