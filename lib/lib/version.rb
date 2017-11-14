3ad37ed70baf308c296a923eaf60a1e4e47a0728
Nick Landgrebe <nick@landgre.be>
2017-11-14 15:01:16 -0600

    Add version file

---
+++ lib/lib/version.rb
@@ -0,0 +1,5 @@
+module Occsn
+  module StickyBooking
+    VERSION = '0.1.0'
+  end
+end
--- lib/sticky_booking.rb
+++ lib/sticky_booking.rb
@@ -1,7 +1,5 @@
 module Occsn
   module StickyBooking
-    VERSION = '0.1.0'
-
     class Engine < ::Rails::Engine
       isolate_namespace StickyBooking
     end
