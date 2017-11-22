3b7141680257d755c387949ea33c94a5faf1ce92
Nick Landgrebe <nick@landgre.be>
2017-11-22 12:47:24 -0500

    Fix redeemable incorporation and refactor price display to add more detailed attributes

--- src/services/occasionSDK.service.js
+++ src/services/occasionSDK.service.js
@@ -128,13 +128,13 @@ angular.module('StickyBooking')
         }
 
         this.queryBuildCard = (token) => {
-            return occsn.CreditCard.build({ id: token });
+            return this.occsn.CreditCard.build({ id: token });
         }
 
         this.queryRedeemableType = (redeemable) => {
-            if( redeemable.isA(occsn.Coupon) )
+            if( redeemable.isA(this.occsn.Coupon) )
                 return 'coupon';
-            if( redeemable.isA(occsn.GiftCard) )
+            if( redeemable.isA(this.occsn.GiftCard) )
                 return 'card';
         }
 
