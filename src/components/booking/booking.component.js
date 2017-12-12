c8ac0063d0ed80ef6ec357a47469dcb0547c56b3
Nick Landgrebe <nick@landgre.be>
2017-12-12 17:03:59 -0600

    Remove all custom date handling in favor of moment objects that can be easily manipulated

--- src/components/booking/booking.component.js
+++ src/components/booking/booking.component.js
@@ -1,5 +1,6 @@
 var angular = require('angular');
 var moment = require('moment');
+var _ = require('underscore');
 var templateUrl = require('ngtemplate-loader!./booking.component.html');
 
 //Creating bookingPage component on StickyBooking Module
@@ -71,14 +72,6 @@ angular.module('StickyBooking')
 
                           $scope.timeSlots = timeSlots;
 
-                          //Find all possible durations
-                          $scope.durations = [];
-                          $scope.timeSlots.map( (timeSlot) => {
-                              if($scope.durations.indexOf(timeSlot.attributes().duration) == -1){
-                                  $scope.durations.push(timeSlot.attributes().duration);
-                              }
-                          });
-
                           //Manually refresh DOM
                           console.log("Calendar data loaded");
                           $scope.calendarDataLoaded = true;
@@ -88,8 +81,7 @@ angular.module('StickyBooking')
                           $scope.$broadcast('timeSlotDataLoaded', {
                               merchant: $scope.merchant,
                               product: $scope.product,
-                              timeSlots: $scope.timeSlots,
-                              durations: $scope.durations
+                              timeSlots: $scope.timeSlots
                           });
                       });
 
@@ -142,27 +134,8 @@ angular.module('StickyBooking')
 
           //When date is selected from calendar
           $scope.$on('dateSelectedEvent', function(event, data){
-              $scope.friendlyDate = data.friendlyDate;
               $scope.selectedDate = data.selectedDate;
-              $scope.selectedDateElement = data.selectedDateElement;
-
-              $scope.availableSlots = [];
-              $scope.timeSlots.map( (timeSlot) => {
-                  if( $scope.sameDay( new Date(timeSlot.startsAt), new Date($scope.selectedDate.stringDate) )){
-                      $scope.availableSlots.push(timeSlot);
-                  }
-              });
-              $scope.availableSlots.sort(function(a,b){
-                  return new Date(b.startsAt) - new Date(a.startsAt);
-              });
-              $scope.availableSlots.reverse();
-          });
-
-          //When new time slots are loaded
-          $scope.$on('timeSlotsUpdated', function(event, data){
-              $scope.timeSlots = data.timeSlots;
-              $scope.displayLoading = false;
-              $scope.$apply();
+              $scope.availableSlots = data.availableTimeSlots;
           });
 
           //When loading animation is started from sub component
@@ -176,27 +149,30 @@ angular.module('StickyBooking')
           });
 
           //When time slot is selected
-          $scope.onTimeSlotSelection = function(event, passTime){
+          $scope.onTimeSlotSelection = function(event, timeSlot){
               event.preventDefault();
-              let time = passTime;
-              $scope.selectedTimeSlot = time;
-              $scope.selectedTimeSlotElement = event.currentTarget;
-              $(".time-slot-buttons button").removeClass("time-slot-active");
-              $scope.selectedTimeSlotElement.className += " time-slot-active";
+              $scope.selectedTimeSlot = timeSlot;
 
-              $scope.order.timeSlots().target().push($scope.selectedTimeSlot);
+              $scope.order.timeSlots().assign([$scope.selectedTimeSlot]);
 
               if($scope.orderLoaded){
                   $scope.startOrder();
               }else{
                   $scope.displayLoading = true;
-                  $scope.$watch('orderLoaded', function(newValue, oldValue, scope){
+                  $scope.$watch('orderLoaded', function(newValue){
                       if(newValue){
                           $scope.displayLoading = false;
                           $scope.startOrder();
                       }
                   });
               }
+          };
+
+          $scope.isActiveTimeSlot = function(timeSlot) {
+              let activeTimeSlot = $scope.order.timeSlots().target().first();
+              if(_.isUndefined(activeTimeSlot)) return false;
+
+              return activeTimeSlot.startsAt.isSame(timeSlot.startsAt);
           };
 
           // Returns a number formatted like "($NN.NN)"
@@ -632,38 +608,34 @@ angular.module('StickyBooking')
           $scope.scrollToAnchor = function(aid){
               var aTag = $("a[name='"+ aid +"']");
               $('html, body').animate( { scrollTop: aTag.offset().top }, 'slow');
-          }
+          };
 
-          //Check to see if two dates are on the same day
-          $scope.sameDay = function(d1, d2) {
-              return d1.getFullYear() === d2.getFullYear() &&
-                      d1.getMonth() === d2.getMonth() &&
-                      d1.getDate() === d2.getDate();
-          }
-
-          //Return a readble time portion of a date
+          //Return a readable time portion of a date
           $scope.formatToTime = function(dateString){
               return moment(dateString).format('LT');
-          }
+          };
 
+          //Return a readable date portion of a date
+          $scope.formatToDate = function(dateString){
+            return moment(dateString).format('MMMM DD, YYYY');
+          };
+
+          //Return a readable full datetime
           $scope.formatToFullDatetime = function(dateString) {
             return moment(dateString).format('dddd MMMM Do, YYYY h:mm A');
-          }
+          };
 
           //Determine which time of day section this timeSlot belongs in
-          $scope.splitByTimeOfDay = function(date, time) {
+          $scope.splitByTimeOfDay = function(timeSlot, time) {
               switch(time){
                   case('morning'):
-                      return new Date(date).getHours() < 12;
-                      break;
+                      return timeSlot.startsAt.hour() < 12;
                   case('afternoon'):
-                      return new Date(date).getHours() >= 12 && new Date(date).getHours() < 18;
-                      break;
+                      return timeSlot.startsAt.hour() >= 12 && timeSlot.startsAt.hour() < 18;
                   case('evening'):
-                      return new Date(date).getHours() >= 18;
-                      break;
+                      return timeSlot.startsAt.hour() >= 18;
               }
-          }
+          };
 
       } //End Controller
 }); //End Component
--- src/components/calendar/calendar.component.js
+++ src/components/calendar/calendar.component.js
@@ -1,286 +1,121 @@
 var angular = require('angular');
 var moment = require('moment');
+var { extendMoment } = require('moment-range');
+var _ = require('underscore');
 var templateUrl = require('ngtemplate-loader!./calendar.component.html');
+
+// add moment-range
+moment = extendMoment(moment);
 
 //Creating bookingCalendar component on the StickyBooking Module
 angular.module('StickyBooking')
     .component('bookingCalendar', {
         templateUrl: templateUrl,
         controller: function($scope, occasionSDKService){
+            $scope.setTimeSlotsForMonth = function(month, timeSlots) {
+                $scope.timeSlotsByMonth[month.format('MM-YYYY')] = timeSlots;
+            };
+
+            $scope.getTimeSlotsForMonth = function(month) {
+                return $scope.timeSlotsByMonth[month.format('MM-YYYY')];
+            };
 
             this.$onInit = function(){
                 console.log("Calendar Component Init");
-                //Configure calendar to display up to 6 months from today
-                let startDate = new Date ( (1 + new Date().getMonth()) + '/01/' + new Date().getFullYear() );
-                let endDate = new Date(startDate);
-                endDate.adjust('months', 6);
-                endDate.adjust('days', -1);
+                $scope.calendarWeeks = {};
+                $scope.timeSlotsByMonth = {};
 
                 //Set starting month and year for the calendar to display
-                $scope.activeCalendarMonth = new Date().getMonth();
-                $scope.activeCalendarYear = new Date().getFullYear();
-                $scope.highestMonthLoaded = $scope.activeCalendarMonth;
-                $scope.highestYearLoaded = $scope.activeCalendarYear;
-
-                //Set max and min range for the calendar in terms of month and year
-                $scope.minCalendarMonth = startDate.getMonth();
-                $scope.minCalendarYear = startDate.getFullYear();
-                $scope.maxCalendarMonth = endDate.getMonth();
-                $scope.maxCalendarYear = endDate.getFullYear();
+                $scope.activeCalendarMonth = moment().startOf('month');
 
                 $scope.$on('timeSlotDataLoaded', function(event, data){
                     $scope.merchant = data.merchant;
                     $scope.product = data.product;
-                    $scope.timeSlots = data.timeSlots;
-                    $scope.durations = data.durations;
-                    //$scope.timeSlotsByMonthArray = data.timeSlotsByMonthArray;
-
-                    //Build calendar object array based on startDate and endDate from above
-                    $scope.allDatesArrayLoaded = false;
-                    $scope.buildAllDatesArray(startDate, endDate, function(){
-                        console.log("All Dates Array Configured", $scope.allDates);
-                        $scope.allDatesArrayLoaded = true;
-                        $scope.$apply();
-                    });
-
+                    $scope.setTimeSlotsForMonth($scope.activeCalendarMonth, data.timeSlots);
                 });
 
-            }
+            };
 
-            //Function takes in the first and last date of booking availability and
-            //generates an object array representing the dates that must be printed
-            //in the calendar on the page
-            $scope.buildAllDatesArray = function(startDate, endDate, callbackFromBuild){
-                $scope.allDates = [];
+            $scope.getCalendarWeeksForMonth = function(month) {
+                if(!$scope.calendarWeeks[month.format('MM-YYYY')]) {
+                  let startDate = moment(month).startOf('month').startOf('week');
+                  let endDate = moment(month).endOf('month').endOf('week');
 
-                let iterationMonth = null;
-                let iterationYear = null;
-                let monthCount = 0;
-                let yearCount = 0;
-
-                //For the range between startDate and endDate, with an interval of days, stepping 1 day at a time
-                startDate.each(endDate, 'days', 1, function(currentDate, currentStep, thisDate){
-                    let newMonth = false;
-                    let newYear = false;
-                    let startDate = false;
-
-                    //Conditionally detect if this date is the start of a month or year
-                    if(iterationMonth == null){
-                        startDate = true;
-                        iterationMonth = new Date(thisDate).getMonth();
-                        iterationYear = new Date(thisDate).getFullYear();
-                        if(new Date(thisDate).getDate() == 1){
-                            newMonth = true;
-                            if(new Date(thisDate).getMonth() == 0){
-                                newYear = true;
-                            }
-                        }
-                    }else{
-                        if(iterationMonth < new Date(currentDate).getMonth()){
-                            iterationMonth++;
-                            newMonth = true;
-                        }
-                        if(iterationMonth > new Date(currentDate).getMonth()){
-                            iterationMonth = 0;
-                            newYear = true;
-                            newMonth = true;
-                        }
+                  var weeks = [];
+                  var currentWeek = 0;
+                  _.each(Array.from(moment.range(startDate, endDate).by('days')), function(moment) {
+                    if(moment.day() == 0) {
+                      weeks[currentWeek] = [moment];
+                    } else {
+                      weeks[currentWeek].push(moment);
                     }
-                    if(startDate){
-                        $scope.allDates.push( { year: new Date(currentDate).getFullYear(), months: [] } );
-                        $scope.allDates[yearCount].months.push( { month: new Date(currentDate).getMonth(), monthName: new Date(currentDate).getMonthName(), days: [] } );
-                    }else{
-                        if(newYear){
-                            $scope.allDates.push( { year: new Date(currentDate).getFullYear(), months: [] } );
-                            yearCount++;
-                            monthCount = 0;
-                            if(newMonth){
-                                $scope.allDates[yearCount].months.push( { month: new Date(currentDate).getMonth(), monthName: new Date(currentDate).getMonthName(), days: [] } )
-                            }
-                        }else{
-                            if(newMonth){
-                                $scope.allDates[yearCount].months.push( { month: new Date(currentDate).getMonth(), monthName: new Date(currentDate).getMonthName(), days: [] } )
-                                monthCount++;
-                            }
-                        }
-                    }
-                    $scope.allDates[yearCount].months[monthCount].days.push({
-                        stringDate: currentDate.toString(),
-                        dayOfWeek: new Date(currentDate).getDay(),
-                        date: new Date(currentDate).getDate()
-                    });
-                });
-                //Create paginated weeks array inside the allDates array
-                $scope.paginateWeeks(() => {
-                    //Run the callback from build function
-                    callbackFromBuild();
-                });
-            }
 
-            //Adds an object of paginated weeks to the allDates array under a month object
-            $scope.paginateWeeks = function(callback){
-                let weekPages = [];
-                let week = [];
-
-                //Loop through each year
-                for(var y = 0; y < $scope.allDates.length; y++){
-                    //Loop through each month
-                    for(var m = 0; m < $scope.allDates[y].months.length; m++){
-                        //Loop through each day
-                        for(var d = 0; d < $scope.allDates[y].months[m].days.length; d++){
-                            let thisDay = $scope.allDates[y].months[m].days[d];
-                            week.push( { thisDay } );
-                            //If this is the last day of the week but not of the month
-                            if( thisDay.dayOfWeek == 6 && thisDay.date < $scope.allDates[y].months[m].days[$scope.allDates[y].months[m].days.length - 1].date ){
-                                weekPages.push( week );
-                                week = [];
-                            }
-                            //If this is the last day of the month
-                            if(d == $scope.allDates[y].months[m].days.length - 1){
-                                weekPages.push( week );
-                                $scope.allDates[y].months[m].weeks = weekPages;
-                                weekPages = [];
-                                week = [];
-                            }
-                        }
+                    if(moment.day() == 6) {
+                      currentWeek++;
                     }
+                  });
+
+                  $scope.calendarWeeks[month.format('MM-YYYY')] = weeks;
                 }
-                //Run callback function
-                callback();
-            }
+
+              return $scope.calendarWeeks[month.format('MM-YYYY')];
+            };
 
             //Moves the activeCalendar month forward to display the next month
             $scope.moveMonthAhead = function(){
-                if( $scope.activeCalendarMonth < 11 ){
-                    if( $scope.activeCalendarYear < $scope.maxCalendarYear ){
-                        $scope.activeCalendarMonth++;
-                        $scope.getNewTimeSlots();
-                    }else if( $scope.activeCalendarYear == $scope.maxCalendarYear ){
-                        if( ($scope.activeCalendarMonth + 1) <= $scope.maxCalendarMonth ){
-                            $scope.activeCalendarMonth++;
-                            $scope.getNewTimeSlots();
-                        }
-                    }
-                }else{
-                    if( (0 <= $scope.maxCalendarMonth) && (($scope.activeCalendarYear + 1) <= $scope.maxCalendarYear) ){
-                        $scope.activeCalendarMonth = 0;
-                        $scope.activeCalendarYear++;
-                        $scope.getNewTimeSlots();
-                    }
-                }
-            }
+                $scope.activeCalendarMonth.add(1, 'month');
+                $scope.getNewTimeSlots();
+            };
 
             //Moves the activeCalendar month back to display the previous month
             $scope.moveMonthBack = function(){
-                if($scope.activeCalendarMonth > 0){
-                    if( $scope.activeCalendarYear > $scope.minCalendarYear ){
-                        $scope.activeCalendarMonth--;
-                    }else if( $scope.activeCalendarYear == $scope.minCalendarYear ){
-                        if( ($scope.activeCalendarMonth - 1) >= $scope.minCalendarMonth){
-                            $scope.activeCalendarMonth--;
-                        }
-                    }
-                }else if( $scope.activeCalendarMonth == 0 ){
-                    if( (11 >= $scope.minCalendarMonth) && ( ($scope.activeCalendarYear - 1) >= $scope.minCalendarYear) ){
-                        $scope.activeCalendarMonth = 11;
-                        $scope.activeCalendarYear--;
-                    }
-                }
-            }
+                $scope.activeCalendarMonth.subtract(1, 'month');
+            };
 
             //Gets new month of time slots on month change
             $scope.getNewTimeSlots = function(){
-                if( ($scope.activeCalendarMonth > $scope.highestMonthLoaded && $scope.activeCalendarYear == $scope.highestYearLoaded) || $scope.activeCalendarYear > $scope.highestYearLoaded){
+                if(!_.has($scope.timeSlotsByMonth, $scope.activeCalendarMonth.format('MM-YYYY'))){
                     $scope.$emit('startLoading');
 
-                    var desiredMonth = moment([$scope.activeCalendarYear, $scope.activeCalendarMonth, 1]);
-                    occasionSDKService.getTimeSlotsByMonth($scope.product, desiredMonth)
-                        .then( (newTimeSlots) => {
+                    occasionSDKService.getTimeSlotsByMonth($scope.product, $scope.activeCalendarMonth)
+                        .then((newTimeSlots) => {
                             console.log("Time slots by month", newTimeSlots);
-                            $scope.timeSlots.push(...newTimeSlots.toArray());
-                            $scope.highestMonthLoaded = $scope.activeCalendarMonth;
-                            $scope.highestYearLoaded = $scope.activeCalendarYear;
-                            $scope.$emit('timeSlotsUpdated', { timeSlots: $scope.timeSlots});
+                            $scope.setTimeSlotsForMonth($scope.activeCalendarMonth, newTimeSlots);
                             $scope.$apply();
                         })
                         .catch( (error) => console.log("Error", error) );
                 }
-            }
+            };
 
             //Evaluates what classes should be applied to the date to distinguish availability
-            $scope.getDisplayClasses = function(passDay){
-                let day = passDay.thisDay;
-                let dayOfWeek = day.dayOfWeek;
-                let classString = '';
+            $scope.getDisplayClassForDay = function(day){
+                if(day.isBefore($scope.activeCalendarMonth) || day.isAfter(moment($scope.activeCalendarMonth).endOf('month'))) {
+                    return 'empty-date';
+                } else if(day.isBefore(moment(), 'day')) {
+                    return 'unavailable-day';
+                } else {
+                    var timeSlotForDay = $scope.getTimeSlotsForMonth($scope.activeCalendarMonth).detect(function(timeSlot) {
+                        return day.isSame(timeSlot.startsAt, 'day');
+                    });
 
-                //Set class depending on if there is a session that day
-                $scope.timeSlots.map( (timeSlot) => {
-                    if( $scope.sameDay( new Date(timeSlot.startsAt), new Date(day.stringDate) )){
-                        classString = 'available-time-slot';
+                    if(!_.isUndefined(timeSlotForDay)) {
+                        var str = 'available-time-slot';
+                        if($scope.selectedDate && $scope.selectedDate.isSame(day, 'day')) {
+                            str += ' selected-date';
+                        }
+
+                        return str;
+                    } else {
+                        return 'unavailable-day';
                     }
-                });
-
-                //Set days before today to be unavailable
-                if( new Date(day.stringDate) < new Date() ){
-                    classString = "unavailable-day";
                 }
-
-                //Return the class to be applied to the element
-                return classString;
-            }
-
-            //Evaluates if the date should be clickable
-            $scope.getClickableStatus = function(passDay){
-                let day = passDay.thisDay;
-                let dayOfWeek = day.dayOfWeek;
-                let clickable = false;
-
-                //Set day as clickable if there is a session that day
-                $scope.timeSlots.map( (timeSlot) => {
-                    if( $scope.sameDay( new Date(timeSlot.startsAt), new Date(day.stringDate) )){
-                        clickable = true;
-                    }
-                });
-
-                //Set days before today as unavailable
-                if( new Date(day.stringDate) < new Date() ){
-                    clickable = false;
-                }
-
-                //Return the clickable status
-                return clickable;
-            }
-
-            //Check to see if two dates are on the same day
-            $scope.sameDay = function(d1, d2) {
-                return d1.getUTCFullYear() === d2.getUTCFullYear() &&
-                       d1.getUTCMonth() === d2.getUTCMonth() &&
-                       d1.getUTCDate() === d2.getUTCDate();
-            }
-
-            //Returns a collection of a given length
-            $scope.returnRange = function(n) {
-                var range = [];
-                for(var i = 0; i < n; i++){
-                    range.push(i);
-                }
-                return range;
-            }
+            };
 
             //When date is selected from calendar
-            $scope.onDateSelection = function(event, passDay){
-                let day = passDay.thisDay;
-                if( $scope.getClickableStatus(passDay) ){
-                    if( $scope.selectedDateElement != null ){
-                        $scope.selectedDateElement.className = "available-time-slot";
-                    }
-
-                    $scope.friendlyDate = new Date(day.stringDate).formatDate();
+            $scope.onDateSelection = function(day){
+                if($scope.getDisplayClassForDay(day) == 'available-time-slot'){
                     $scope.selectedDate = day;
-                    $scope.selectedDateElement = event.currentTarget;
-                    $scope.selectedDateElement.className += " selected-date";
 
-                    $(".time-slot-buttons button").removeClass("time-slot-active");
                     $('.available-times').fadeIn();
                     $scope.scrollToAnchor('time-slot-scroll');
                     $("#booking-process-status .booking-step-1").addClass("booking-step-complete").removeClass("booking-step-active");
@@ -288,19 +123,23 @@ angular.module('StickyBooking')
                     $("#booking-process-status .booking-step-2").addClass("booking-step-complete").removeClass("booking-step-active");
                     $("#booking-process-status .booking-step-3").addClass("booking-step-active");
 
+                    var availableTimeSlotsForDay =
+                        $scope.getTimeSlotsForMonth($scope.activeCalendarMonth).select(function(timeSlot) {
+                            return timeSlot.startsAt.isSame(day, 'day');
+                        });
+
                     $scope.$emit('dateSelectedEvent', {
-                        friendlyDate: $scope.friendlyDate,
                         selectedDate: $scope.selectedDate,
-                        selectedDateElement: $scope.selectedDateElement,
+                        availableTimeSlots: availableTimeSlotsForDay
                     });
                 }
-            }
+            };
 
             //Scroll to specified anchor tag
             $scope.scrollToAnchor = function(aid){
                 var aTag = $("a[name='"+ aid +"']");
                 $('html, body').animate( { scrollTop: aTag.offset().top }, 'slow');
-            }
+            };
 
         } //End Controller
 
